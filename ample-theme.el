fb6cf3142048255ea9077a61901efee70beb519a
Kevin "The Nuclear" Bloom <kevin@Eponine.local>
2016-05-06 15:46:03 -0400

    Uploaded ample-theme file

---
+++ ample-theme.el
@@ -0,0 +1,40 @@
+;; This file contains the changes that I've made to the Ample theme for emacs.
+;; The only reason why I made this was because if it updates, I want to know
+;; what I need to change.
+
+;; Color palette
+((ample/green "#6aaf50")
+      (ample/dark-green "#057f40")
+      (ample/blue "#5180b3")
+      (ample/blue-bg "#102843")
+      (ample/light-blue "#528fd1")
+      (ample/lighter-blue "#68a5e9")
+      (ample/orange "#dF9522")
+      (ample/tan "#bdbc61")
+      (ample/dark-tan "#7d7c61")
+      (ample/yellow "#baba36")
+      (ample/bright-yellow "#fffe0a")
+      (ample/purple "#ab75c3")
+      (ample/gray "#757575")
+      (ample/dark-gray "#656565")
+      (ample/darker-gray "#454545")
+      (ample/darkest-gray "#252525")
+      (ample/red "#cd5542")
+      (ample/dark-red "#9d2512")
+
+      (ample/cursor "#ffffff")
+      (ample/fringe "#1f1f1f")
+      (ample/region "#696969")
+
+      (ample/rb0 "#81b0e3")
+      (ample/rb1 "#a5a5a5")
+      (ample/rb2 "#6190c3")
+      (ample/rb3 "#959595")
+      (ample/rb4 "#4170a3")
+      (ample/rb5 "#757575")
+
+      (ample/bg "gray13")
+      (ample/fg "#bdbdb3"))
+
+;; mini buff
+`(minibuffer-prompt ((t (:foreground ,ample/red :bold t :background nil))))
